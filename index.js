f951babd4cd71e65f64464fd0a1c84eb9ceb03df
abdulkarim70 <mmd.abdulkarim@gmail.com>
2026-06-25 21:53:40 +0600

    updated

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 app.use(
   cors({
     origin: ["http://localhost:3000",
-      "https://bibliodrop-client-zeta.vercel.app/"
+      "https://bibliodrop-client-zeta.vercel.app"
     ],
   })
 );
